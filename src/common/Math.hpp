f74ddf31cc3c823557406b3c8e6292a38f7c3e04
Vladislav Gerasimov <gerasimovvs@yahoo.com>
2017-07-24 08:57:58 -0700

    [DGE-1] Support for float values was added to json_dto (#7)

--- src/common/Math.hpp
+++ src/common/Math.hpp
@@ -1,4 +1,5 @@
 #pragma clang diagnostic push
+#pragma ide diagnostic ignored "CannotResolve"
 #pragma ide diagnostic ignored "OCUnusedGlobalDeclarationInspection"
 #pragma ide diagnostic ignored "ClangTidyInspection"
 #pragma ide diagnostic ignored "HidingNonVirtualFunction"
@@ -9,152 +10,40 @@
 
 #include "Types.hpp"
 
+#define V2 math::vec::_2
+#define V3 math::vec::_3
+#define V4 math::vec::_4
+
 namespace math {
 
     namespace vec {
 
-        class _2 {
+        class _2 : public glm::vec2 {
         public:
-            _2() :
-                    x(0), y(0) {}
-
-            _2(double x, double y) :
-                    x(x), y(y) {}
-
-            _2(double x) :
-                    x(x), y(x) {}
-
-            _2(std::initializer_list<double> l) {
-                auto s = std::vector<double>(l);
-                switch (s.size()) {
-                    case 0:
-                        break;
-                    case 1:
-                        x = s[0];
-                        y = s[0];
-                        break;
-                    default:
-                        x = s[0];
-                        y = s[1];
-                        break;
-                }
-            }
-
-            double x;
-            double y;
-
-            glm::vec2 toGlm() {
-                return glm::vec2(x, y);
-            }
+            using glm::vec2::vec2;
         };
 
-        class _3 {
+        class _3 : public glm::vec3 {
         public:
-            _3() :
-                    x(0), y(0), z(0) {}
-
-            _3(double x, double y, double z) :
-                    x(x), y(y), z(z) {}
-
-            _3(double x) :
-                    x(x), y(x), z(x) {}
-
-            _3(std::initializer_list<double> l) {
-                auto s = std::vector<double>(l);
-                switch (s.size()) {
-                    case 0:
-                        break;
-                    case 1:
-                        x = s[0];
-                        y = s[0];
-                        z = s[0];
-                        break;
-                    case 2:
-                        x = s[0];
-                        y = s[1];
-                        z = s[1];
-                        break;
-                    default:
-                        x = s[0];
-                        y = s[1];
-                        z = s[2];
-                        break;
-                }
-            }
-
-            double x;
-            double y;
-            double z;
-
-            glm::vec3 toGlm() {
-                return glm::vec3(x, y, z);
-            }
+            using glm::vec3::vec3;
         };
 
-        class _4 {
+        class _4 : public glm::vec4 {
         public:
-            _4() :
-                    x(0), y(0), z(0), w(0) {}
-
-            _4(double x, double y, double z, double w) :
-                    x(x), y(y), z(z), w(w) {}
-
-            _4(double x) :
-                    x(x), y(x), z(x), w(x) {}
-
-            _4(std::initializer_list<double> l) {
-                auto s = std::vector<double>(l);
-                switch (s.size()) {
-                    case 0:
-                        break;
-                    case 1:
-                        x = s[0];
-                        y = s[0];
-                        z = s[0];
-                        w = s[0];
-                        break;
-                    case 2:
-                        x = s[0];
-                        y = s[1];
-                        z = s[1];
-                        w = s[1];
-                        break;
-                    case 3:
-                        x = s[0];
-                        y = s[1];
-                        z = s[2];
-                        w = s[2];
-                        break;
-                    default:
-                        x = s[0];
-                        y = s[1];
-                        z = s[2];
-                        w = s[3];
-                        break;
-                }
-            }
-
-            double x;
-            double y;
-            double z;
-            double w;
-
-            glm::vec4 toGlm() {
-                return glm::vec4(x, y, z, w);
-            }
+            using glm::vec4::vec4;
         };
     }
 
     namespace color {
 
-        class rgb : public vec::_3 {
+        class rgb : public glm::vec3 {
         public:
-            using vec::_3::_3;
+            using glm::vec3::vec3;
         };
 
-        class rgba : public vec::_4 {
+        class rgba : public glm::vec4 {
         public:
-            using vec::_4::_4;
+            using glm::vec4::vec4;;
         };
     }
 }
--- src/runner/EngineRunner.cpp
+++ src/runner/EngineRunner.cpp
@@ -3,9 +3,9 @@
 void EngineRunner::run() {
 
     using namespace math;
-    
+
     auto transform =
-            model::component::Transform({1.0, 2.0, 3.0}, {1, 2}, {10});
+            model::component::Transform(V3{1.0, 2.0, 3.0}, V3{5, 1, 2}, V3{10});
 
     auto s = serialize::JsonSerializer::serialize(transform, true);
     LOG(INFO) << s;
